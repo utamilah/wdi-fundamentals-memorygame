942f8ddd4775af65886e3b9519443d1367f82ed2
utamilah <halibakarr@Halimatus-MBP.fios-router.home>
2017-03-15 23:24:30 -0400

    created if statement and alerts

--- js/main.js
+++ js/main.js
@@ -4,3 +4,16 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+	alert("Sorry, try again.")
+  } 
+  else if (cardOne === cardThree) {
+  	alert("Sorry, try again.")
+  } 
+  else if (cardOne === cardThree) {
+  	alert("Sorry, try again.")
+  } 
+  else { 
+  	alert("You found a match!")
+}
