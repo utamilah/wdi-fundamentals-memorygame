eebaf1ba971653dd58f57f4434a574471bf4964b
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-03-19 22:08:24 -0400

    add img selector, comment out card divs, get card game to work, add image files to root folder

--- js/main.js
+++ js/main.js
@@ -1,19 +1,62 @@
-console.log("JS file is connected to HTML! Woo!")
+//Create array for cards
+var cards = ['queen', 'queen', 'king', 'king'];
+//Create empty array that will contain cards in play
+var cardsInPlay = [];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+//Select game board
+var gameBoard = document.getElementById('game-board');
 
-if (cardTwo === cardFour) {
-	alert("Sorry, try again.")
-  } 
-  else if (cardOne === cardThree) {
-  	alert("Sorry, try again.")
-  } 
-  else if (cardOne === cardThree) {
-  	alert("Sorry, try again.")
-  } 
-  else { 
-  	alert("You found a match!")
+function createBoard() {
+  for (var i=0; i<cards.length; i++) {
+  	var cardElement = document.createElement('div');
+  	
+  	//Add card class to div once created
+  	cardElement.className = 'card';
+  	//assign the data-card attribute to individual cards
+    cardElement.setAttribute('data-card', cards[i]);
+
+    cardElement.addEventListener('click', isTwoCards);
+
+    //add the cards to the parent board
+  	gameBoard.appendChild(cardElement);
+  	gameBoard.appendChild(cardElement);
+  }
 }
+
+createBoard();
+
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='king.png'>";
+	} else {
+		this.innerHTML = "<img src='queen.png'>";
+	}
+
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+
+}
+
+function isMatch(cards) {
+	if (cards[0] === cards[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
+
+/*if (cardOne === cardTwo) {
+	alert("You found a match!")
+  } else if (cardThree === cardFour) {
+  	alert("You found a match!")
+  } else {
+	alert("Sorry, try again.")
+  }
+}*/
+
